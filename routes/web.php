41a33dddaf055c072c6b43ccf97df5c66baab075
ASUS <mitashya_ct@students.amikom.ac.id>
2026-05-08 07:45:23 +0700

    Tugas 4

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // --- Halaman Utama (Home) ---
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // --- Rute Wajib  ---
 Route::get('/profil', function () {
